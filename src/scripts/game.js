55c7f16082378dd0edd4919c3ad8e9d06186ec38
Mustafa Kurukamci <isbuan51@gmail.com>
2020-04-02 15:15:52 +0200

    added Heads Up and highscorepage

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,12 +1,14 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById('questionCounterText');
+const scoreText = document.getElementById('scoreText');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuesions = [];
+let availableQuestions = [];
 let questions = [{
-        question: "Inside which HTML element do we put the JavaScript??",
+        question: "Inside which HTML element do we put the JavaScript?",
         choice1: "<script>",
         choice2: "<javascript>",
         choice3: "<js>",
@@ -33,27 +35,29 @@ let questions = [{
 //*******************//
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
-startGame = () => {
+const startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuesions = [...questions];
-    console.log(availableQuesions);
+    availableQuestions = [...questions];
+    //console.log(availableQuestions);
     getNewQuestion();
 };
-getNewQuestion = () => {
-    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+const getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         return window.location.assign("/end.html");
     }
     questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-    currentQuestion = availableQuesions[questionIndex];
+    questionCounterText.innerHTML = `${questionCounter} / ${MAX_QUESTIONS}`;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    //console.log(currentQuestion);
     question.innerText = currentQuestion.question;
     choices.forEach(choice => {
         const number = choice.dataset["number"];
         choice.innerText = currentQuestion["choice" + number];
     });
-    availableQuesions.splice(questionIndex, 1);
-    console.log(availableQuesions);
+    availableQuestions.splice(questionIndex, 1);
+    //console.log(availableQuestions);
     acceptingAnswers = true;
 };
 choices.forEach(choice => {
@@ -62,8 +66,22 @@ choices.forEach(choice => {
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset["number"];
-        console.log(selectedAnswer);
-        getNewQuestion();
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+        //console.log(classToApply);
+        //console.log(selectedAnswer);
+        if (classToApply === 'correct') incrementScore(CORRECT_BONUS);
+        selectedChoice.parentElement.classList.add(classToApply);
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1500);
+
     });
 });
-startGame();
+
+const incrementScore = number => {
+    score += number;
+    scoreText.innerHTML = score;
+}
+
+export default startGame;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,19 +1,69 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
 import '../styles/main.scss';
 // Import any additional modules you want to include below \/
-
+import startGame from './game';
 
 // \/ All of your javascript should go here \/
 
+//Hide main page and display game page when we click on the play button
 const playButton = document.querySelector('#play');
 const homePage = document.querySelector('#home');
 const gamePage = document.querySelector('#game-page');
 
-
-const displayGamePage = (e) => {
+const displayGamePageAndStartGame = (e) => {
     e.preventDefault();
     homePage.style.display = 'none';
     gamePage.style.display = 'flex';
+    startGame();
 }
 
-playButton.addEventListener('click', displayGamePage);
+playButton.addEventListener('click', displayGamePageAndStartGame);
+
+
+
+// go High Score page
+const highScoresPage = document.querySelector('#highScores-page');
+const highScoresButton = document.querySelector('#highScores-btn');
+
+const displayHighScoresPageAndStartGame = (e) => {
+    e.preventDefault();
+    homePage.style.display = 'none';
+    highScoresPage.style.display = 'flex';
+}
+
+highScoresButton.addEventListener('click', displayHighScoresPageAndStartGame);
+
+
+//goHomeButton
+const homePageButton = document.querySelector('#goHome-btn');
+
+const displayHomePage = (e) => {
+    e.preventDefault();
+    homePage.style.display = 'flex';
+    highScoresPage.style.display = 'none';
+}
+
+homePageButton.addEventListener('click', displayHomePage);
+
+// highScore
+const highScoresList = document.getElementById("highScoresList");
+
+//const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+//Temporary
+const highScores = [{
+    score: 100,
+    name: "Felix"
+}, {
+    score: 90,
+    name: "Joe"
+}, {
+    score: 150,
+    name: "Steve"
+}];
+
+
+//console.log(highScores);
+highScoresList.innerHTML = highScores.map(score => {
+        return `<li class="high-score">${score.name} => ${score.score}</li>`;
+    })
+    .join("");
